bc99de4e94a9579577fab58caea39a5281f129b3
Juan Riquelme González <juanriqgon@protonmail.com>
2018-01-13 18:48:41 +0100

    feature: future state persistence feat prototype

---
+++ state/utils/persistence.js
@@ -0,0 +1,23 @@
+/* import { AsyncStorage } from 'react-native';
+import { APP_STORAGE_KEY } from '../../constants'; */
+
+/**
+ * Function to load the app state from AsyncStorage.
+ * @returns {Promise} Promise object that represents the state persisted
+ * in the device.
+ */
+/* const loadState = () => AsyncStorage.getItem(APP_STORAGE_KEY); */
+
+/**
+ * Function to persist the app state into AsyncStorage.
+ * @returns {Promise}
+ */
+/* const persistState = (state) => {
+  const serializedState = JSON.stringify(state);
+  return AsyncStorage.mergeItem(APP_STORAGE_KEY, serializedState);
+}
+
+export {
+  loadState,
+  persistState,
+}; */
